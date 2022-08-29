658b617ca6f25164679f6e88d1ac6da631297175
moamen <moamen.zakaria@gmail.com>
2022-08-29 22:41:36 +0200

    skeleton

---
+++ src/main/java/labforwardwordlookupapi/LabforwardWordLookupApiApplication.java
@@ -0,0 +1,13 @@
+package labforwardwordlookupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabforwardWordLookupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabforwardWordLookupApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/labforwardwordlookupapi/LabforwardWordLookupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package labforwardwordlookupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabforwardWordLookupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
